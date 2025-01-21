c62ac94836e67db00fe7c075da4b0e20c26f8ebb
Adroaldo <hygino@msn.com>
2025-01-21 07:47:08 -0300

    Check Box

--- app/src/main/java/br/edu/utfpradroaldoferreira/PessoaActivity.java
+++ app/src/main/java/br/edu/utfpradroaldoferreira/PessoaActivity.java
@@ -4,6 +4,7 @@ import static android.widget.Toast.LENGTH_LONG;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PessoaActivity extends AppCompatActivity {
     private EditText editTextNome, editTextMedia;
+    private CheckBox checkBoxBolsista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +21,14 @@ public class PessoaActivity extends AppCompatActivity {
 
         editTextNome = findViewById(R.id.editTextNome);
         editTextMedia = findViewById(R.id.editTextMedia);
+        checkBoxBolsista = findViewById(R.id.checkBoxBolsista);
     }
 
     public void limparCampos(View view) {
         editTextNome.setText(null);
         editTextMedia.setText(null);
-        //coloca o cursor sobre o elemento
+        checkBoxBolsista.setChecked(false);
         editTextNome.requestFocus();
-
         Toast.makeText(this, R.string.campos_limpos, LENGTH_LONG).show();
     }
 
@@ -38,7 +40,7 @@ public class PessoaActivity extends AppCompatActivity {
 
             //retorna o foco
             editTextNome.requestFocus();
-           // editTextNome.setSelection(0, editTextNome.getText().toString().length());
+            // editTextNome.setSelection(0, editTextNome.getText().toString().length());
 
             return;
         }
@@ -48,28 +50,33 @@ public class PessoaActivity extends AppCompatActivity {
         if (mediaString == null || mediaString.trim().isEmpty()) {
             Toast.makeText(this, R.string.faltou_entrar_com_a_media, LENGTH_LONG).show();
             editTextMedia.requestFocus();
-           // editTextMedia.setSelection(0, editTextMedia.getText().toString().length());
             return;
         }
 
         nome = nome.trim();
-
+        int media = 0;
         try {
-            int media = Integer.parseInt(mediaString);
+            media = Integer.parseInt(mediaString);
 
             if (media < 0 || media > 100) {
                 Toast.makeText(this, R.string.a_media_informada_tem_um_valor_invalido, LENGTH_LONG).show();
                 editTextMedia.requestFocus();
                 //seleciona a parte que está com erro
                 editTextMedia.setSelection(0, editTextMedia.getText().toString().length());
-            } else {
-                Toast.makeText(this, getString(R.string.nome_valor) + nome + '\n' + getString(R.string.media_valor) + media, LENGTH_LONG).show();
+                return;
             }
         } catch (NumberFormatException e) {
             Toast.makeText(this, R.string.valor_numerico_invalido, LENGTH_LONG).show();
             editTextMedia.requestFocus();
             editTextMedia.setSelection(0, editTextMedia.getText().toString().length());
         }
-    }
 
+        boolean bolsista = checkBoxBolsista.isChecked();
+
+        Toast.makeText(this,
+                getString(R.string.nome_valor) + nome + '\n'
+                        + getString(R.string.media_valor) + media + '\n'
+                        + (bolsista ? getString(R.string.possui_bolsa) : getString(R.string.nao_possui_bolsa)),
+                LENGTH_LONG).show();
+    }
 }
