801b6fd92025dbf20c7a85164474af8cf1263e32
Adroaldo <hygino@msn.com>
2025-02-05 14:13:37 -0300

    Abertura de Activity com Intent Implícita

--- app/src/main/java/br/edu/utfpradroaldoferreira/SobreActivity.java
+++ app/src/main/java/br/edu/utfpradroaldoferreira/SobreActivity.java
@@ -1,6 +1,10 @@
 package br.edu.utfpradroaldoferreira;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +16,47 @@ public class SobreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sobre);
 
         setTitle(R.string.sobre);
+    }
+
+    public void abrirSiteAutoria(View view) {
+        String endereco = "http://lattes.cnpq.br/3956885446716361";
+        abrirSite(endereco);
+    }
+
+    private void abrirSite(String endereco) {
+        Intent intentAbertura = new Intent(Intent.ACTION_VIEW);
+
+        intentAbertura.setData(Uri.parse(endereco));
+
+        if (intentAbertura.resolveActivity(getPackageManager()) != null) {
+            startActivity(intentAbertura);
+        } else {
+            Toast.makeText(this,
+                            R.string.nenhum_aplicativo_para_abrir_paginas_web
+                            , Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    public void enviarEmailAutor(View view) {
+        enviarEmail(
+                new String[]{"hygino@msn.com"},
+                "Contato pelo aplicativo");
+    }
+
+    public void enviarEmail(String[] enderecos, String assunto) {
+        Intent intentAbertura = new Intent(Intent.ACTION_SENDTO);
+        intentAbertura.setData(Uri.parse("mailto:"));
+        intentAbertura.putExtra(Intent.EXTRA_EMAIL, enderecos);
+        intentAbertura.putExtra(Intent.EXTRA_SUBJECT, assunto);
+
+        if (intentAbertura.resolveActivity(getPackageManager()) != null) {
+            startActivity(intentAbertura);
+        } else {
+            Toast.makeText(this,
+                            R.string.nenhum_aplicativo_para_enviar_e_mail
+                            , Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 }
